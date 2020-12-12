3d2dcb8130e82ea0b5dea83ebe142402fb34e7d9
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2020-12-12 20:31:09 +0100

    Finish the game (develop)

--- Assets/Scripts/MenuManager.cs
+++ Assets/Scripts/MenuManager.cs
@@ -5,27 +5,60 @@ using UnityEngine.SceneManagement;
 
 public class MenuManager : MonoBehaviour
 {
+    public Canvas MenuCanvas, MapCanvas, ShopCanvas;
+    public UnityEngine.UI.Image desertImage, highwayImage, marsImage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MenuCanvas.enabled = true;
+        MapCanvas.enabled = false;
+        ShopCanvas.enabled = false;
+
+        if(PlayerPrefs.GetInt("desert") == 0) desertImage.color = new Color(1, 1, 1, 0.3f);
+        if(PlayerPrefs.GetInt("highway") == 0) highwayImage.color = new Color(1, 1, 1, 0.3f);
+        if(PlayerPrefs.GetInt("mars") == 0) marsImage.color = new Color(1, 1, 1, 0.3f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(PlayerPrefs.GetInt("desert") == 1) desertImage.color = new Color(255, 255, 255, 255);
+        if(PlayerPrefs.GetInt("highway") == 1) highwayImage.color = new Color(255, 255, 255, 255);
+        if(PlayerPrefs.GetInt("mars") == 1) marsImage.color = new Color(255, 255, 255, 255);
     }
 
     public void game(){
-    	SceneManager.LoadScene("ForestScene");
+    	MenuCanvas.enabled = false;
+        MapCanvas.enabled = true;
     }
 
     public void shop(){
+        MenuCanvas.enabled = false;
+        ShopCanvas.enabled = true;
+    }
 
+    public void back(){
+        SceneManager.LoadScene("MainMenuScene");
     }
 
     public void exit(){
     	Application.Quit();
+    }
+
+    public void forest(){
+        SceneManager.LoadScene("ForestScene");
+    }
+
+    public void desert(){
+        if(PlayerPrefs.GetInt("desert") == 1) SceneManager.LoadScene("DesertScene");
+    }
+
+    public void highway(){
+        if(PlayerPrefs.GetInt("highway") == 1) SceneManager.LoadScene("HighwayScene");
+    }
+
+    public void mars(){
+        if(PlayerPrefs.GetInt("mars") == 1) SceneManager.LoadScene("MarsScene");
     }
 }
---
+++ Assets/Scripts/ShopManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    public UnityEngine.UI.Image desertImage, highwayImage, marsImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("desert") == 0) desertImage.color = new Color(1, 1, 1, 0.3f);
+        if(PlayerPrefs.GetInt("highway") == 0) highwayImage.color = new Color(1, 1, 1, 0.3f);
+        if(PlayerPrefs.GetInt("mars") == 0) marsImage.color = new Color(1, 1, 1, 0.3f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(PlayerPrefs.GetInt("desert") == 1) desertImage.color = new Color(255, 255, 255, 255);
+        if(PlayerPrefs.GetInt("highway") == 1) highwayImage.color = new Color(255, 255, 255, 255);
+        if(PlayerPrefs.GetInt("mars") == 1) marsImage.color = new Color(255, 255, 255, 255);
+    }
+
+    public void desert(){
+    	int coins = PlayerPrefs.GetInt("coins");
+
+    	if(coins >= 200 && (PlayerPrefs.GetInt("desert") == 0)){
+    		PlayerPrefs.SetInt("desert", 1);
+    		PlayerPrefs.SetInt("coins", coins - 200);
+    	}
+    }
+
+    public void highway(){
+    	int coins = PlayerPrefs.GetInt("coins");
+
+    	if(coins >= 2500 && (PlayerPrefs.GetInt("highway") == 0)){
+    		PlayerPrefs.SetInt("highway", 1);
+    		PlayerPrefs.SetInt("coins", coins - 2500);
+    	}
+    }
+
+    public void mars(){
+    	int coins = PlayerPrefs.GetInt("coins");
+
+    	if(coins >= 12000 && (PlayerPrefs.GetInt("mars") == 0)){
+    		PlayerPrefs.SetInt("mars", 1);
+    		PlayerPrefs.SetInt("coins", coins - 12000);
+    	}
+    }
+}
