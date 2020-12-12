1d75d3b1a80be3426dec0d85a4a49471d2188083
Miroslav Grofčík <miroslav.grofcik@demi.sk>
2020-12-12 13:19:34 +0100

    Add gas (develop)

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,13 +4,19 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
-	public Rigidbody2D car_body;
-	public Rigidbody2D back_tire;
-	public Rigidbody2D front_tire;
+    public Rigidbody2D car_body;
+    public Rigidbody2D back_tire;
+    public Rigidbody2D front_tire;
 
-	public float carTorque = 10;
-	public float speed = 100;
-	private float movement;
+    public float carTorque = 1000;
+    public float speed = 100;
+    public float movement;
+
+    public float fuel = 1;
+    public float fuelConsumption = 0.1f;
+    public UnityEngine.UI.Image fuelImage;
+
+    public GasScript gas;
 
     // Start is called before the first frame update
     void Start()
@@ -21,13 +27,20 @@ public class CarController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement = Input.GetAxis("Horizontal");
+        movement = gas.holding ? 1 : 0;
+        fuelImage.fillAmount = fuel;
     }
 
-    private void FixedUpdate() {
-		front_tire.AddTorque(-movement * speed * Time.fixedDeltaTime);
-       	back_tire.AddTorque(-movement * speed * Time.fixedDeltaTime); 
-       	car_body.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
-	}
+    void FixedUpdate()
+    {
+        if(fuel > 0)
+        {
+            front_tire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            back_tire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            car_body.AddTorque(-movement * carTorque * Time.fixedDeltaTime);
+
+            fuel -= fuelConsumption * Mathf.Abs(movement) * Time.fixedDeltaTime;
+        }
+    }
 
 }
---
+++ Assets/Scripts/GasScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GasScript : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    public Sprite onDown;
+    public Sprite onUp;
+    public Image image;
+
+    public bool holding = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        holding = true;
+        image.sprite = onDown;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        holding = false;
+        image.sprite = onUp;
+    }
+}
